43de3c01509a3e0370c473fa0e60386645494164
Md Sagor <mdsagorahmed1224@gmail.com>
2026-08-28 09:44:19 +0600

    sg3

--- src/data/natoks.js
+++ src/data/natoks.js
@@ -69,7 +69,7 @@ export const natoks = [
     title: 'Sukher Golpo | Full Drama | Jovan | Keya Payel | Eid Natok',
     slug: 'Sukher-Golpo',
     thumbnail: '/images/natok-4.webp',
-    videoUrl: 'https://www.youtube.com/embed/37p6_DJivt4',
+    videoUrl: 'https://www.youtube.com/embed/37p6_DJivt4?start=1646',
     category: 'Drama',
     date: '2026-08-18',
     trending: true
